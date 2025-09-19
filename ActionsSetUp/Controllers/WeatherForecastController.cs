10db3b4e4943045a2029f2333741e49aaa1a7e58
FrankBatista09 <120326134+FrankBatista09@users.noreply.github.com>
2025-09-18 23:11:01 -0400

    Update weather summary text for clarity

--- ActionsSetUp/Controllers/WeatherForecastController.cs
+++ ActionsSetUp/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace ActionsSetUp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Klk no te haga por favor. si"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Klk no te haga por favor. si no"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
